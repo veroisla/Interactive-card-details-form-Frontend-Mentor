50542bfe82f0e740363967e9d77377c915699e71
Veronica isla <veroislagleaniz@gmail.com>
2022-09-14 14:20:25 +0200

    display input date and cvc

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,22 +4,65 @@ import ConfirmButton from '../components/ConfirmButton';
 
 function Form() {
   return (
-    <section>
+    <section className="form">
       <form action="" method="post" className="form__sectionForm">
-        <label htmlFor="name">cardholder name</label>
-        <input type="text" name="name" id="name" placeholder="Jane Appleseed" />
-        <label htmlFor="card number">card number</label>
+        <label className="form__label" htmlFor="name">
+          cardholder name
+        </label>
         <input
+          className="form__input"
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Jane Appleseed"
+        />
+        <label className="form__label" htmlFor="card number">
+          card number
+        </label>
+        <input
+          className="form__input"
           type="text"
           name="card number"
           id="card number"
           placeholder="1234 5678 9123 0000"
         />
-        <label htmlFor="exp. date">Exp. Date (MM/YY)</label>
-        <input type="text" name="exp. date" id="exp. date" placeholder="MM" />
-        <input type="text" name="exp. date" id="exp. date" placeholder="YY" />
-        <label htmlFor="cvc">CVC</label>
-        <input type="text" name="cvc" id="cvc" placeholder="123" />
+        <div className="form__containerDateAndCvc">
+          <div className="form__input--containerDate">
+            <label className="form__label" htmlFor="exp. date">
+              Exp. Date (MM/YY)
+            </label>
+            <div className="form__date">
+              <input
+                className="form__input form__input--date"
+                type="text"
+                name="exp. date"
+                id="exp. date"
+                placeholder="MM"
+              />
+              <input
+                className="form__input form__input--date"
+                type="text"
+                name="exp. date"
+                id="exp. date"
+                placeholder="YY"
+              />
+            </div>
+          </div>
+          <div className="form__containerCvc">
+            {' '}
+            <label className="form__label" htmlFor="cvc">
+              CVC
+            </label>
+            <input
+              className="form__input form__input--cvc"
+              type="text"
+              name="cvc"
+              id="cvc"
+              placeholder="123"
+            />
+          </div>
+        </div>
+
         <ConfirmButton />
       </form>
     </section>
