c9bf247f745bc38bd2c82e3b484a0e1cce274d18
Veronica isla <veroislagleaniz@gmail.com>
2022-10-09 21:26:37 +0200

    correct issues with desktop responsive version

--- src/components/CardPreview.js
+++ src/components/CardPreview.js
@@ -5,37 +5,39 @@ import cardLogo from '../images/card-logo.svg';
 function CardPreview(props) {
   return (
     <section className="preview">
-      <div className="preview__cardsContainer">
-        {/* PREVIEW CARD FRONT */}
-        <div className="preview__cardFront preview__card">
-          <img className="preview__cardLogo" src={cardLogo} alt="Card Logo" />
-          <span className="preview__cardNumber">
-            {props.dataUser.cardNumber
-              .replace(/\s/g, '')
-              .replace(/\D/g, '')
-              //dejar hueco entre numeros tarjeta
-              .replace(/([0-9]{4})/g, '$1 ')
+      <div className="preview__backgroundPurple">
+        <div className="preview__cardsContainer">
+          {/* PREVIEW CARD FRONT */}
+          <div className="preview__cardFront preview__card">
+            <img className="preview__cardLogo" src={cardLogo} alt="Card Logo" />
+            <span className="preview__cardNumber">
+              {props.dataUser.cardNumber
+                .replace(/\s/g, '')
+                .replace(/\D/g, '')
+                //dejar hueco entre numeros tarjeta
+                .replace(/([0-9]{4})/g, '$1 ')
 
-              .trim() || '0000 0000 0000 0000'}
-          </span>
-          <div className="preview__nameAndDate">
-            <span className="preview__name">
-              {props.dataUser.name || 'JANE APLESEED'}
+                .trim() || '0000 0000 0000 0000'}
             </span>
-            <div>
-              <span className="preview__date">
-                {props.dataUser.month || '00'}
+            <div className="preview__nameAndDate">
+              <span className="preview__name">
+                {props.dataUser.name || 'JANE APLESEED'}
               </span>
-              /
-              <span className="preview__date">
-                {props.dataUser.year || '00'}
-              </span>
+              <div>
+                <span className="preview__date">
+                  {props.dataUser.month || '00'}
+                </span>
+                /
+                <span className="preview__date">
+                  {props.dataUser.year || '00'}
+                </span>
+              </div>
             </div>
           </div>
-        </div>
-        {/* PREVIEW CARD BACK */}
-        <div className="preview__cardBack preview__card">
-          <span className="preview__cvc">{props.dataUser.cvc || '000'}</span>
+          {/* PREVIEW CARD BACK */}
+          <div className="preview__cardBack preview__card">
+            <span className="preview__cvc">{props.dataUser.cvc || '000'}</span>
+          </div>
         </div>
       </div>
     </section>
